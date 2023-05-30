6fd80b28bd5496f4f6c95cc0e35d5cff3e100f96
unknown <toshconsultinc@gmail.com>
2023-05-30 07:01:08 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+function initForm(root){
+    const email = root.querySelector('.email-input');
+    const form = root.querySelector('form');
+    const warning = root.querySelector('.warning');
+    
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      warning.classList.add('hidden');
+      email.classList.remove('invalid');
+      form.reset();
+    });  
+   
+    email.addEventListener('invalid', (e) => {
+      e.preventDefault();
+      warning.classList.remove('hidden');
+      email.classList.add('invalid');
+    });
+  }
+  
+  document.querySelectorAll('.form-container').forEach(initForm);
